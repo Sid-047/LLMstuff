e105f5c47e887ce9b1c8f4b334cb72248bcae064
Sid <siddiqmoideen07@gmail.com>
2023-12-03 20:25:14 +0530

    CPU Utilization Stuff

--- DocumentQnA/magic.py
+++ DocumentQnA/magic.py
@@ -1,9 +1,11 @@
 from transformers import pipeline
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 nlp = pipeline(
     "document-question-answering",
     model="impira/layoutlm-document-qa",
-)
+).to(device)
 
 nlp(
     "#DocImgFile",
