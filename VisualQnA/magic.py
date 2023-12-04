083b95c6d478e6122ebad1d0c6a7f085b59d369e
Sid <siddiqmoideen07@gmail.com>
2023-12-04 10:01:23 +0530

    Initiated Prototyping

--- VisualQnA/magic.py
+++ VisualQnA/magic.py
@@ -1,15 +1,23 @@
+import time
 import torch
 from PIL import Image
+from tkinter import filedialog
 from transformers import BlipProcessor, BlipForQuestionAnswering
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
-model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base").to("cuda" if torch.cuda.is_available() else "cpu")
+t1 = time.time()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Select the Directory Yo!")
+inImg = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.gif *.bmp *.tiff")])
+rawImg = Image.open(inImg).convert('RGB')
 
-imgPath = 'imgSample.png'
-raw_image = Image.open(imgPath).convert('RGB')
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base").to(device)
 
 question = "how many peeps are in this Picture?"
-inputs = processor(raw_image, question, return_tensors="pt").to("cuda" if torch.cuda.is_available() else "cpu")
+inputs = processor(rawImg, question, return_tensors="pt").to(device)
 
 out = model.generate(**inputs)
 print(processor.decode(out[0], skip_special_tokens=True))
+
+t2 = time.time()
+print("\nCompleteExecTime: ", (t2-t1))
