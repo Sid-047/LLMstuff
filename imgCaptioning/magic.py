0e31ac74d6e3a6392456b11c00c26ae073209b4a
Sid <siddiqmoideen07@gmail.com>
2024-04-10 00:53:58 +0530

    LoaDeD imgCaptioning Stuff

---
+++ imgCaptioning/magic.py
@@ -0,0 +1,38 @@
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from tkinter import filedialog
+from PIL import Image
+import torch
+import tqdm
+import glob
+import time
+
+t1 = time.time()
+print("Select the imgDirectory Yo!")
+imgDir = filedialog.askdirectory() + '//'
+print("---->", imgDir)
+
+print("Select the Out captionDirectory Yo!")
+capDir = filedialog.askdirectory() + '//'
+print("---->", capDir)
+
+x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
+x = sum(x , [])
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to(device)
+
+for i in tqdm.tqdm(x, desc = "Captionin' the Images Yo!", colour = 'red'):
+    rawImg = Image.open(i).convert('RGB')
+    inputs = processor(rawImg, return_tensors="pt").to(device)
+    out = model.generate(**inputs)
+    genOut = processor.decode(out[0], skip_special_tokens=True).replace('there is a', 'a')
+    print(genOut)
+    i = i.replace(imgDir, capDir)
+    i = ''.join([str(x) for x in i.split('.')[:-1]])
+    f = open(i + '.txt', 'w')
+    f.write(genOut)
+    f.close()
+
+t2 = time.time()
+print("\nCompleteExecTime: ", (t2-t1))
